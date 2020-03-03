092c2768f6509748c66bd64915d978762267bfe7
Valéria Nicéria <valerianiceria@gmail.com>
2020-03-03 09:29:38 -0300

    update

--- code/processamento_linguagem_natural.R
+++ code/processamento_linguagem_natural.R
@@ -1,13 +1,40 @@
 # Instalando os pacotes
-# install.packages("rtweet")
-# install.packages("tm")
-# install.packages("wordcloud")
 # install.packages("tidyverse")
+# install.packages("rtweet")
+# install.packages("udpipe", dependencies = T)
 
 # Carregando os pacotes
-library(tm)
-library(wordcloud)
 library(tidyverse)
+library(rtweet)
+library(udpipe)
 
-# Lendo dados do resultado da busca da api do Twitter
-datascience_tweet <- readRDS("dados/dados_busca_twitter.rds")
+# Buscando os tweets com a #datascience em português
+datascience_tweets <- search_tweets(
+  "#datascience",
+  n = 100,
+  include_rts = FALSE,
+  lang = "pt-br"
+)
+
+View(datascience_tweets)
+
+arquivo <- data.frame(FILE = datascience_tweets$text)
+arquivo$ID = 1:length(arquivo$FILE)
+
+View(arquivo)
+
+# Download do modelo
+modelo = udpipe::udpipe_download_model(language = "portuguese")
+
+# Carregando o modelo baixado
+modelo = udpipe::udpipe_load_model("portuguese-bosque-ud-2.4-190531.udpipe")
+
+# Anotações no formato CoNLL-U
+anotar <- 
+  udpipe::udpipe_annotate(
+    modelo,
+    x = arquivo$FILE,
+    doc_id = arquivo$ID
+  ) %>% as.data.frame()
+
+View(anotar)
